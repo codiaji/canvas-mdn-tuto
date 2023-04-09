e27db27ab828a48b22e706ff849f6f04a2ef46e8
codiaji <codiaji@gmail.com>
2023-04-09 11:57:58 +0000

    An example with support checking

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+const canvas = document.getElementById('tutorial');
+if (canvas.getContext) {
+	const context = canvas.getContext('2d');
+	context.fillStyle = 'red';
+	context.fillRect(10, 10, 50, 50);
+	context.fillStyle = 'rgba(0,0,200,0.5)';
+	context.fillRect(30, 30, 50, 50);
+}
